ad12834da839598770c1cbb13ec24c65c13e0952
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-09-24 20:00:42 +0700

    trying to reset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./main/*.{html,js}"],
+  content: [],
   theme: {
-    extend: {
-      padding: {
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-      },
-      gap: {
-        '4': '2rem',
-      }
-
-    },
+    extend: {},
   },
   plugins: [],
 }
