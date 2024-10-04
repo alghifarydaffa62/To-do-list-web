687d4ffc52e066b2ab1b8a568323f4ae97a8c554
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-10-04 09:46:53 +0700

    tugas feature

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
 
+function saveToLocalStorage(judul, deadline, desc) {
+    const data = { judul, deadline, desc };
+
+    let savedData = JSON.parse(localStorage.getItem('data')) || [];
+    savedData.push(data);
+    localStorage.setItem('data', JSON.stringify(savedData));
+}
+
 function tugas() {
     const judul = prompt("Masukkan Nama Matkul: ")
     const deadline = prompt("Masukkan Deadline: ")
     const desc = prompt("Deskripsi Tugas: ")
 
+    createtugas(judul, deadline, desc)
+    saveToLocalStorage(judul, deadline, desc)
+}
+
+function createtugas(judul, deadline, desc) {
     const matkul = document.createElement("h1")
     matkul.textContent = "Matkul: " + judul
     matkul.classList.add("text-xl")
@@ -17,7 +30,16 @@ function tugas() {
     deskripsi.id = "desc"
     deskripsi.classList.add("break-words")
     deskripsi.classList.add("overflow-y-auto")
-    
+
+    const button = document.createElement("button")
+    button.textContent = "Selesai"
+    button.classList.add("p-1")
+    button.classList.add("text-md")
+    button.classList.add("font-semibold")
+    button.classList.add("bg-slate-700")
+    button.classList.add("rounded-md")
+    button.classList.add("hover:bg-slate-800")
+
     const box = document.createElement("div")
     box.style.width = '25vw'
     box.style.height = '30vh'
@@ -31,7 +53,56 @@ function tugas() {
     box.appendChild(matkul)
     box.appendChild(dl)
     box.appendChild(deskripsi)
+    box.appendChild(button)
 
     const container = document.getElementById("container")
     container.appendChild(box)
+}
+
+function loadFromLocalStorage() {
+    const savedData = JSON.parse(localStorage.getItem('data')) || [];
+    savedData.forEach(data => {
+        createtugas(data.judul, data.deadline, data.desc);
+    });
+    
+}
+window.onload = function() {
+    loadFromLocalStorage();
+};
+
+function materi() {
+    const judulmateri = prompt("Masukkan Nama Matkul: ")
+    const keterangan = prompt("Masukkan keterangan: ")
+    const link = prompt("Masukkan Link: ")  
+
+    const mapel = document.createElement("h1")
+    mapel.textContent = "Matkul: " + judulmateri
+    mapel.classList.add("text-xl")
+    mapel.classList.add("font-semibold")
+
+    const ket = document.createElement("h2")
+    ket.textContent = keterangan
+
+    const desk = document.createElement("p")
+    desk.textContent = link
+    desk.id = "desc"
+    desk.classList.add("break-words")
+    desk.classList.add("overflow-y-auto")
+
+    const boxmateri = document.createElement("div")
+    boxmateri.style.width = '25vw'
+    boxmateri.style.height = '30vh'
+    boxmateri.classList.add("bg-blue-950")
+    boxmateri.classList.add("rounded-md")
+    boxmateri.classList.add("p-6")
+    boxmateri.classList.add("flex")
+    boxmateri.classList.add("flex-col")
+    boxmateri.classList.add("gap-2")
+
+    boxmateri.appendChild(mapel)
+    boxmateri.appendChild(ket)
+    boxmateri.appendChild(desk)
+
+    const containermateri = document.getElementById("materi-container")
+    containermateri.appendChild(boxmateri)
 }
